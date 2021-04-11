8ca8ed74a754e8d97c32a7d9b857c64896820296
Dima Vyshniakov <morewings@gmail.com>
2021-04-11 17:44:47 +0200

    wip

---
+++ packages/eslint/.stylelintrc.cjs
@@ -0,0 +1,4 @@
+// In this package context this config is just dummy to silence Webstorm false error reports
+module.exports = {
+  ignoreFiles: ['**/*.snap', '**/*.json', '**/*.png', '**/*.md', '**/*.woff', '**/*.ttf', '**/*.eot']
+}
