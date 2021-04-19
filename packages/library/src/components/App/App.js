90abd7989bfa0fa13aa9596b697b7b4a20e0afad
Dima Vyshniakov <morewings@gmail.com>
2021-04-19 09:59:39 +0200

    WIP

--- packages/library/src/components/App/App.js
+++ packages/library/src/components/App/App.js
@@ -5,10 +5,15 @@ import * as classes from './App.module.css';
 
 export const App = () => (
   <div className={classes.test}>
-    <div>
-      <Input />
+    <div className={classes.fragment}>
+      <Input
+        className="lol"
+        onChange={() => {
+          console.log('hello!');
+        }}
+      />
     </div>
-    <div>
+    <div className={classes.fragment}>
       <Button variant="primary" label="Hello world!" />
     </div>
   </div>
--- packages/library/src/components/Input/Input.js
+++ packages/library/src/components/Input/Input.js
@@ -1,3 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {useVariable} from 'css-vars-hook';
+import * as classes from './Input.module.css';
 
-export const Input = () => <input type="text" />;
+export const Input = ({state, value, type, className, ...props}) => {
+  const inputColor = {
+    pristine: 'blue',
+    valid: 'green',
+    error: 'red',
+  }[state];
+  const {setRef, style} = useVariable('color', inputColor);
+  return (
+    <input
+      ref={setRef}
+      style={style}
+      className={classes.input}
+      type={type}
+      {...props}
+    />
+  );
+};
+
+Input.propTypes = {
+  state: PropTypes.oneOf(['error', 'pristine', 'valid']),
+  type: PropTypes.oneOf(['text', 'number', 'file']),
+  value: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+Input.defaultProps = {
+  state: 'pristine',
+  type: 'text',
+  className: '',
+};
