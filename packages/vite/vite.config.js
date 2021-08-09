c685dafd3b4491dc42a219472ad917ccefd128b5
Dima Vyshniakov <morewings@gmail.com>
2021-08-09 10:45:41 +0200

    add vite

--- packages/vite/vite.config.js
+++ packages/vite/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import {defineConfig} from 'vite';
+import path from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
